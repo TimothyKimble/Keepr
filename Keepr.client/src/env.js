3fde3ef21dbe9fafe9d7e8ee1b7c06ca1247362a
Timothy Kimble <timble417@gmail.com>
2021-09-10 11:59:42 -0600

    initialized Repo

---
+++ Keepr.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'dev-ci5b8144.us.auth0.com'
+export const audience = 'https://Keeper.com'
+export const clientId = 'Uu2bKer2ZgtT7NPTNUUox8lQ1hBLWRiY'
